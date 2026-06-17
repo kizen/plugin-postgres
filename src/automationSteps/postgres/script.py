584a201798323e5a1bba6b066897f4f8c260e79b
smfKizen <scott.filips@kizen.com>
2026-06-17 13:35:17 -0500

    CREATE secrets, inputs and output result variables

--- src/automationSteps/postgres/script.py
+++ src/automationSteps/postgres/script.py
@@ -1,5 +1,3 @@
-import secrets
-
 import psycopg
 from psycopg.rows import tuple_row
 
@@ -7,13 +5,31 @@ def connect_to_postgres():
     connection = None
     try:
         outputs.log(f'Secrets: {secrets}')
-        
+
+        secret_password = next(iter(key for key in secrets if key.endswith("postgres_password")), None)
+        POSTGRES_PASSWORD = secrets[secret_password]
+
+        secret_port = next(iter(key for key in secrets if key.endswith("postgres_port")), None)
+        POSTGRES_PORT = secrets[secret_port]
+
+        secret_host = next(iter(key for key in secrets if key.endswith("postgres_host")), None)
+        POSTGRES_HOST = secrets[secret_host]
+
+        INPUT_USER = inputs.user
+        outputs.log(f"Input user: {INPUT_USER}")
+
+        INPUT_DATABASE = inputs.database
+        outputs.log(f"Input database: {INPUT_DATABASE}")
+
+        INPUT_QUERY = inputs.query
+        outputs.log(f"Input query: {INPUT_QUERY}")
+
         connection = psycopg.connect(
-            host="localhost",
-            dbname="TEST_DB", # Note: 'dbname' instead of 'database'
-            user="scott_readonly",
-            password="test12345",
-            port="5432",
+            host=POSTGRES_HOST,
+            dbname=INPUT_DATABASE,
+            user=INPUT_USER,
+            password=POSTGRES_PASSWORD,
+            port=POSTGRES_PORT,
             row_factory=tuple_row # Matches psycopg2 behavior: returns tuples
         )
 
@@ -21,11 +37,12 @@ def connect_to_postgres():
         with connection.cursor() as cursor:
             # 3. Execute a sample test query
             outputs.log("Successfully connected to the database!")
-            cursor.execute("""SELECT t."Age" FROM public.test_table t;""")
+            cursor.execute(INPUT_QUERY)
 
             # 4. Fetch and display the results
-            db_version = cursor.fetchone()
-            outputs.log(f"PostgreSQL database version: {db_version[0]}")
+            query_result = cursor.fetchone()
+            outputs.log(f"PostgreSQL query result: {query_result[0]}")
+            outputs.result = str(query_result[0])  # Store result as string for output
 
         # psycopg v3 autocommits DDL but not DML. For SELECT it's fine.
         # If you did INSERT/UPDATE: connection.commit()
